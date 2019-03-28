340538b8e76a186593135f438c43d96dc54b910b
Courtenay <nay.adams@gmail.com>
2019-03-27 18:53:10 -0700

    Completed large hero block and intro content block

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,2 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-
-gulp.task('default', function() {
-    console.log("Hurray—you create a Gulp task.");
-});
-
-gulp.task('html', function() {
-  console.log("Something useful to HTML!");
-});
-
-gulp.task('styles', function() {
-  console.log("Sass or Post CSS tasks running here!");
-});
-
-
-gulp.task('watch', function() {
-
-  watch('./app/index.html', function(){
-    gulp.start('html');
-  });
-
-watch('./app/assets/styles/**/*.css', function() {
-  gulp.start('styles');
-})
-
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
